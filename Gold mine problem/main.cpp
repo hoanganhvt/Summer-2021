4a4c6cd2bf559f01f915286cca64cbdc77cb79ed
Hoang Anh <hoanganhvt.dev@gmail.com>
2021-04-06 22:15:30 +0700

    gold mine go go brrrr

---
+++ Gold mine problem/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorrithm>
+using namespace std;
+
+int gold[100][100];
+
+int maxGold(int m,int n){
+    const int mS = m;
+    const int nS = n;
+    int goldMine[mS][nS];
+    for(int col = n-1; col >= 0;col--){
+        for(int row = 0; row < m;row++){
+           int right = (col == n-1)? 0 : goldMine[row][col+1];
+           int upRight = (col == n-1 || row == 0)? 0 :goldMine[row-1][col+1];
+           int downRight = (col == n-1 || row == m-1) ? 0 : goldMine[row+1][col+1];
+           goldMine[row][col] = max(right,max(upRight,downRight)) + gold[row][col];
+        }
+    }
+
+    int maxVal = goldMine[0][0];
+    for(int i = 0; i < m;i++){
+        maxVal  = max(gold[0][i],maxVal);
+    }
+    return maxVal;
+}
+int main(){
+}
