20842711f618f93897319d09a296723736817882
hoang anh <hoanganhvt.dev@gmail.com>
2021-05-11 21:36:26 +0700

    new file

---
+++ Knapsack/knapRecur.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int Weight[10000];
+int Value[10000];
+int solve(int w,int i){
+    if(Weight[i] > w){
+        return 0;
+    }
+    else if(i == 0){
+        return 0;
+    }
+    return max(solve(w-Weight[i],i-1)+Value[i],solve(w,i-1)); 
+}
+int main(){
+    int N,W;
+    cin >> W >> N;
+    for(int i = 1; i <= N;i++){
+        cin >> Weight[i] >> Value[i];
+    }
+    cout << solve(W,N) << endl;
+}
